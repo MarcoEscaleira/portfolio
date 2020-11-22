ec9e81baf8655c97fadf24702bcb948176084cdc
Marco Escaleira <marco.escaleira@mindera.com>
2020-11-22 23:48:00 +0000

    feat: base commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Marco Escaleira</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to Marco Escaleira Portfolio
+        </h1>
+      </main>
+    </div>
+  )
+}
