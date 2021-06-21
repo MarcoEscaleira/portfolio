1416ec27ba3f4a0b1c0f505f4b8bef2cfb83053e
Marco Escaleira <marco.escaleira@mindera.com>
2021-06-21 02:12:02 +0100

    feat: basic meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,24 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MainDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          <meta name="title" content="Marco Escaleira Portfolio" />
+          <meta
+            name="description"
+            content="My own personal portfolio on web development. A journey full-field of adventures and challenges overcome."
+          />
+          <meta
+            name="keywords"
+            content="porfolio, marco, escaleira, websites, web, frontend, front-end, motojoy"
+          />
+
+          <meta name="robots" content="noindex, nofollow">
+            
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="language" content="English" />
+          <meta name="author" content="Marco Escaleira" />
+
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
