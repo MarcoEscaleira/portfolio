21bad69652c0a56818c35a0549533a0e28d40d0d
Marco <marcoescaleira2000@gmail.com>
2023-08-19 01:08:49 +0100

    crete new template pages and bumping outdated versions

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <div>
+      <h3 className="text-3xl font-bold">
+        About me page
+      </h3>
+    </div>
+  );
+}
---
+++ pages/portfolio.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h3 className="text-3xl font-bold">
+        Marco&apos;s portfolio
+      </h3>
+    </div>
+  );
+}
